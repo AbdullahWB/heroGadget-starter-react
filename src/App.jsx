3246f07d5d0cf604bcce82a556b90c3c0c694c8c
AbdullahWB <abdullah917828@gmail.com>
2023-04-26 16:57:59 +0600

    add sesson stor

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import React, { createContext, useState } from 'react';
 import { Outlet, useLoaderData } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Modal from './components/Model';
 
 export const productContext = createContext([])
 export const CartContext = createContext([])
 
 const App = () => {
+  let [isOpen, setIsOpen] = useState(false)
   const { cartArr, products } = useLoaderData()
   const [cart, setCart] = useState(cartArr)
+
+  const cartAlert = sessionStorage.getItem("alert")
+  if (cart.length > 0 && cartAlert !== 'true') {
+    setIsOpen(true)
+    sessionStorage.setItem('alert', true)
+  }
+
   return (
     <productContext.Provider value={products}>
       <CartContext.Provider value={[cart, setCart]}>
@@ -17,6 +26,7 @@ const App = () => {
           <Outlet></Outlet>
         </div>
         <Footer></Footer>
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen} ></Modal>
       </CartContext.Provider>
     </productContext.Provider>
   );
